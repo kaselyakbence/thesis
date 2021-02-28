1949dd83c5f35ce73f2948a3fe89751fa256079e
Kaselyák Bence <kaselyakbence.elte@gmail.com>
2021-02-28 12:07:23 +0100

    fronted logrocket implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-
+import LogRocket from "logrocket";
+LogRocket.init("txvrtg/thesis");
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>React app!</h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
