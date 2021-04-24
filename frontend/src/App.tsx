f745847437fc1c5eda6e2ca5179af8d1ffb494ff
Kaselyák Bence <kaselyakbence.elte@gmail.com>
2021-04-24 12:19:42 +0200

    typescript pwa setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <p>React app</p>;
-}
+import React, { FC } from "react";
+
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme();
+
+const App: FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <p>React App</p>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
