007cbe4c8f4fe5d79143d62e411238609befcc86
Kaselyák Bence <kaselyakbence.elte@gmail.com>
2021-05-03 17:50:24 +0200

    search  added

--- backend/src/routes/user/search.ts
+++ backend/src/routes/user/search.ts
@@ -8,6 +8,9 @@ import { authorize } from "../../middlewares/authorization-middleware";
 //Models
 import { User } from "../../models/User";
 
+//Errors
+import { UnauthorizedError } from "../../errors/unauthorized-error";
+
 const router = express.Router();
 
 router.post(
@@ -20,13 +23,19 @@ router.post(
   ],
   authorize,
   async (req: Request, res: Response) => {
+    //TODO If requested exclude friend and maybe those who already requested to be friends
+
+    if (!req.currentUser) throw new UnauthorizedError();
+
     const { query } = req.body;
 
     const regex = new RegExp(`.*${query}.*`);
 
     const users = await User.find({ nick_name: regex }).select("nick_name").limit(25).exec();
 
-    res.send(users);
+    const excludedUsers = users.filter((user) => user.nick_name !== req.currentUser?.nick_name);
+
+    res.send(excludedUsers);
   }
 );
 
